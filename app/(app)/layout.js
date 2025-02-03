ba8c8558b4e576c6d11256973caa15085c1ed085
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-02-03 18:56:59 +0530

    updating layout

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <div className="w-screen">
+    <div >
       <Navbar />
       {children}
     </div>
